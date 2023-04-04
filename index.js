be78b29028a77372941defb495ca327fce65c148
Naman Aggarwal <naman.aggarwal2001@gmail.com>
2023-04-04 18:52:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,5 @@ app.get('/', (req, res) => {
 app.post('/add', (req, res) => {
   const {a,b} = req.body;
   res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
 });
 module.exports = app;
